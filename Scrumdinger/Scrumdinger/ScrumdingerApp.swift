a9332e62e40978925d437913c57d6778cf0353b1
Choi WooYoung <djdj7087@gmail.com>
2023-03-30 13:39:43 +0900

    create Scrumdinger project

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 최우영 on 2023/03/30.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
