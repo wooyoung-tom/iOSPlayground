05a0032bc7cf698f4f352c656e06ce4c040b52d0
Choi WooYoung <djdj7087@gmail.com>
2023-03-30 17:03:11 +0900

    Creating a navigation hierarchy

---
+++ Scrumdinger/Scrumdinger/DetailView.swift
@@ -0,0 +1,60 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by 최우영 on 2023/03/30.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info"), content: {
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+                
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    
+                    Spacer()
+                    
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    
+                    Spacer()
+                    
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            })
+            Section(header: Text("Attendees"), content: {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            })
+        }
+        .navigationTitle(scrum.title)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
