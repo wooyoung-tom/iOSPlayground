5011e180d7433c540de0a6cfac8a991a279134a8
Choi WooYoung <djdj7087@gmail.com>
2023-04-03 15:27:03 +0900

    complete first app

--- MyFirstApp/MyFirstApp/ViewController.swift
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -11,13 +11,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mainLabel: UILabel!
 
+    @IBOutlet weak var mainButton: UIButton!
+    
+    // 앱의 화면에 들어오면 처음 실행시키는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         mainLabel.text = "안녕하세요"
-        mainLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        mainLabel.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
     }
 }
